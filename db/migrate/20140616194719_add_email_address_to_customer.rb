18374bd0a9a592209755c0ca6f5d5dd9a40ad6a9
Nathan Hall <nathan.mh@gmail.com>
2014-06-16 15:16:39 -0500

    3 models

---
+++ db/migrate/20140616194719_add_email_address_to_customer.rb
@@ -0,0 +1,10 @@
+class AddEmailAddressToCustomer < ActiveRecord::Migration
+  def up
+    add_column :customers, :email, :string
+  end
+
+  def down
+    remove_column :customers, :email
+  end
+
+end
